70789063df915644d6421800d9969fe3fc323a96
ugbeadie <ugbeadie3@gmail.com>
2023-07-08 21:07:49 +0100

    navbar starts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../style";
+import { NavLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = "";
 
-export default Navbar
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex item-center
+    py-5 fixed top-0 z-0 bg-primary`}
+    >
+      <div
+        className="w-full flex justify-between items-center
+       max-w-7xl mx-auto"
+      >
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9 h-9
+          object-contain"
+          />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
